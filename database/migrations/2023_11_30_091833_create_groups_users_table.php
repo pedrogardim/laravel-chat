e6c97ddf9cbfd271b2f8f08558efe7af0bec922e
Pedro Gardim <me@pedrogardim.com>
2023-11-30 10:28:16 +0100

    feat: add users_group_migration

---
+++ database/migrations/2023_11_30_091833_create_groups_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('groups_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('group_id')->constrained();
+            $table->timestamp('joined_at')->useCurrent();
+            $table->enum('role', ['user', 'admin'])->default('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('groups_users');
+    }
+};
